14d327969a621e640989e796fd210a2b31107f06
Stephen Beer <atlassian@corner.plus.com>
2024-10-08 21:06:46 +0100

    before changing getter and setters in dog-details component

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -17,30 +17,29 @@ export class SettingsComponent {
   constructor(public auth: myAuthService, private themeService: ThemeService) {}
 
   settingsCurrrentDarkMode: string ='';
+  isDarkMode: boolean = false;
+
 
   ngOnInit(): void {
       // Subscribe the appCurrentDarkMode property of theme service to get real time value
       this.themeService.currentDarkMode.subscribe(
         // update the component's property
-        darkMode => this.settingsCurrrentDarkMode = darkMode
+        darkMode => {
+          this.settingsCurrrentDarkMode = darkMode
+          this.isDarkMode = darkMode === 'dark' ? true : false;
+        }
       );
     }
 
   // function to update the darkMode in the service
   submitHandler(){
     this.themeService.updateDarkMode(this.settingsCurrrentDarkMode);
-     this.settingsCurrrentDarkMode="";
+     this.settingsCurrrentDarkMode = "";
    }
 
-  /*toggleTheme() {
-    this.themeService.toggleTheme(); // Use the service to toggle the theme
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    this.themeService.updateDarkMode(this.isDarkMode ? 'dark' : 'light');
   }
 
-  get isDarkMode(): boolean {
-    return this.themeService.isDarkModeEnabled(); // Check if dark mode is enabled
-  }
-
-  get myUImode(): string {
-    return this.themeService.uiMode();
-  }*/
 }
--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -5,12 +5,12 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root',
 })
 export class ThemeService {
-
+  // use of BehaviourSubject learnt from https://medium.com/@mohsinogen/angular-17-data-sharing-with-behaviorsubjects-a-simple-guide-bab56530c832
   constructor(){}
 
   // declare and initialize the darkMode property
   // which will be a BehaviorSubject
-  darkMode = new BehaviorSubject("dark");
+  darkMode = new BehaviorSubject("light");
 
   // expose the BehaviorSubject as an Observable
   currentDarkMode = this.darkMode.asObservable();
@@ -19,25 +19,5 @@ export class ThemeService {
   updateDarkMode(newDarkMode: string){
     this.darkMode.next(newDarkMode);
   }
-
-  /*toggleTheme() {
-    this.isDarkMode.next(!this.isDarkMode.value); // Update to use BehaviorSubject
-    console.log(`Theme toggled. Dark mode is now ${this.isDarkMode.value ? 'enabled' : 'disabled'}.`);
-    //document.body.classList.toggle('dark', this.isDarkMode.value);
-  }
-
-  isDarkModeEnabled(): boolean {
-    console.log('isDarkModeEnabled within theme.service has been called')
-    return this.isDarkMode.getValue(); // Updated to get the boolean value
-  }
-
-  uiMode(): string {
-    console.log('uiMode function within theme.service has been called')
-    return this.isDarkMode ? 'dark' : 'light';
-  }
-
-  get uiModeVal(): string {
-    return this.uiMode();
-}*/
 
 }
