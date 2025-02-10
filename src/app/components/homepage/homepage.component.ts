c528702f7291d233af29946d2e48a3e39fee352d
Stephen Beer <atlassian@corner.plus.com>
2025-02-10 22:38:10 +0000

    working kind of okay if ng server has --no-hmr flag

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { myAuthService } from '../../auth/auth.service';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { NavigationComponent } from '../navigation/navigation.component';
 
 @Component({
     selector: 'app-homepage',
-    imports: [NavigationComponent, RouterOutlet],
+    imports: [],
     templateUrl: './homepage.component.html',
     styleUrl: './homepage.component.css'
 })
