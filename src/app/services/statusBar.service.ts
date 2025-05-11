0ddf661b2d62e9714f37b94addd43aca32dcd3f5
Stephen Beer <atlassian@corner.plus.com>
2025-05-11 22:40:57 +0100

    status bar config stored in local storage

--- src/app/services/statusBar.service.ts
+++ src/app/services/statusBar.service.ts
@@ -8,9 +8,12 @@ export class StatusBarService {
   // use of BehaviourSubject learnt from https://medium.com/@mohsinogen/angular-17-data-sharing-with-behaviorsubjects-a-simple-guide-bab56530c832
   constructor(){}
 
+  //Used to store status bar visibility configuration setting in local storage
+  public visibilityConfigSetting = 'hide';
+
   // declare and initialize the darkMode property
   // which will be a BehaviorSubject
-  statusBarVisibility = new BehaviorSubject("show");
+  statusBarVisibility = new BehaviorSubject(localStorage.getItem(this.visibilityConfigSetting) || 'hide');
 
   // expose the BehaviorSubject as an Observable
   currentVisibility = this.statusBarVisibility.asObservable();
@@ -18,6 +21,7 @@ export class StatusBarService {
   // function to update the value of the BehaviorSubject
   updateVisibility(newVisibility: string){
     this.statusBarVisibility.next(newVisibility);
+    localStorage.setItem(this.visibilityConfigSetting, newVisibility)
   }
 
 }
